be9434d00c9327e2eaf2b168ed4cec174e9ef4f9
DianaMGalindo <95235340+DianaMGalindo@users.noreply.github.com>
2022-01-29 09:47:15 +0000

    Adding remaining tsx files

---
+++ levels/tilesets/background_v2.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Background_v2" tilewidth="3008" tileheight="640" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="3008" height="640" source="../../graphics/background/general_background_v2.jpg"/>
+ </tile>
+</tileset>
---
+++ levels/tilesets/background_v3.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="background_v3" tilewidth="3136" tileheight="640" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="3136" height="640" source="../../graphics/background/general_background_v3.jpg"/>
+ </tile>
+</tileset>
